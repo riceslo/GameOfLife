6f480837327d4dbfec1deac99b64f4cc97b2dc86
riceslo <51858232+riceslo@users.noreply.github.com>
2019-08-29 22:48:18 -0400

    Segunda Prueba

--- GameOfLifeTest/GameOfLifeTest.cs
+++ GameOfLifeTest/GameOfLifeTest.cs
@@ -19,5 +19,17 @@ namespace GameOfLifeTest
 
              Assert.AreEqual(CellStatus.LIVE, result);
         }
+
+        [TestMethod]
+        public void CelulaVivaCon3VecinasVivas()
+
+        {   /*Celula Viva con 3 Vecinas Viva, debe devolver como nuevo estado la celula viva */
+            CellStatus currentStatus = CellStatus.LIVE;
+            int neighborsAlive = 3;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.LIVE, result);
+        }
     }
 }
