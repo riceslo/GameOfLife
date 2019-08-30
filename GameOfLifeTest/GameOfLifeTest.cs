e7811becfb9efbdf550937f29e412e8bffdcb789
riceslo <51858232+riceslo@users.noreply.github.com>
2019-08-29 23:23:44 -0400

    Add files via upload

--- GameOfLifeTest/GameOfLifeTest.cs
+++ GameOfLifeTest/GameOfLifeTest.cs
@@ -57,5 +57,57 @@ namespace GameOfLifeTest
 
             Assert.AreEqual(CellStatus.DEAD, result);
         }
+
+        [TestMethod]
+        public void CelulaVivaCon1VecinasVivas()
+
+        {   /*Celula Viva con 1 Vecinas Viva, debe devolver como nuevo estado la celula muerta */
+            /* muere de soledad */
+            CellStatus currentStatus = CellStatus.LIVE;
+            int neighborsAlive = 1;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.DEAD, result);
+        }
+
+        [TestMethod]
+        public void CelulaMuertaCon1VecinasViva()
+
+        {   /*Celula Muerta con 1 Vecinas Viva, debe devolver como nuevo estado la celula muerta */
+            
+            CellStatus currentStatus = CellStatus.DEAD;
+            int neighborsAlive = 1;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.DEAD, result);
+        }
+
+        [TestMethod]
+        public void CelulaMuertaCon2VecinasViva()
+
+        {   /*Celula Muerta con 2 Vecinas Viva, debe devolver como nuevo estado la celula muerta */
+
+            CellStatus currentStatus = CellStatus.DEAD;
+            int neighborsAlive = 2;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.DEAD, result);
+        }
+
+        [TestMethod]
+        public void CelulaMuertaCon3VecinasViva()
+
+        {   /*Celula Muerta con 3 Vecinas Viva, debe devolver como nuevo estado la celula viva */
+
+            CellStatus currentStatus = CellStatus.DEAD;
+            int neighborsAlive = 3;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.LIVE, result);
+        }
     }
 }
