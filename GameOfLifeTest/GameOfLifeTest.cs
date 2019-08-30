fe008e380d3a661881c43dc7bed67789b5b6963f
riceslo <51858232+riceslo@users.noreply.github.com>
2019-08-29 23:05:14 -0400

    Add files via upload

--- GameOfLifeTest/GameOfLifeTest.cs
+++ GameOfLifeTest/GameOfLifeTest.cs
@@ -35,9 +35,23 @@ namespace GameOfLifeTest
         [TestMethod]
         public void CelulaVivaCon4VecinasVivas()
 
-        {   /*Celula Viva con 3 Vecinas Viva, debe devolver como nuevo estado la celula viva */
+        {   /*Celula Viva con 4 Vecinas Viva, debe devolver como nuevo estado la celula muerta */
+            /* muere de superpoblacion */
             CellStatus currentStatus = CellStatus.LIVE;
             int neighborsAlive = 4;
+
+            CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
+
+            Assert.AreEqual(CellStatus.DEAD, result);
+        }
+
+        [TestMethod]
+        public void CelulaVivaCon5VecinasVivas()
+
+        {   /*Celula Viva con 5 Vecinas Viva, debe devolver como nuevo estado la celula muerta */
+            /* muere de superpoblacion */
+            CellStatus currentStatus = CellStatus.LIVE;
+            int neighborsAlive = 5;
 
             CellStatus result = RulesGameOfLife.GetNewStatus(currentStatus, neighborsAlive);
 
