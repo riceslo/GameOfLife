1a080209558c4f5539b49b72659a57ecb9821851
riceslo <51858232+riceslo@users.noreply.github.com>
2019-08-29 23:06:05 -0400

    Add files via upload

---
+++ GameOfLife/GameOfLife/RulesGameOfLife.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+
+    public enum CellStatus
+    {
+        LIVE = 1,
+        DEAD = 0
+
+    }
+
+    public class RulesGameOfLife
+    {
+
+        //  REGLLAS
+        //  1 PARA QUE UNA CELULA SIGA VIVA, TIENE QUE TENER 2 O 3 VECINAS.SI TIENE MAS MUERE DE SUPER POBLACION.SI TIENE MENOS, MUERE DE SOLEDAD
+        //  2 SI UNA CELULA ESTA MUERTA Y TIENE 3 VECINAS VIVAS REVIVE.
+        
+
+        public static CellStatus GetNewStatus(CellStatus currentStatus, int neighborsAlive) {
+            CellStatus result = currentStatus;
+              
+            if (currentStatus == CellStatus.LIVE && neighborsAlive >= 2 && neighborsAlive <= 3) {
+                result = CellStatus.LIVE;
+            }
+
+            if (currentStatus == CellStatus.LIVE && neighborsAlive > 3) {
+                result = CellStatus.DEAD;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
